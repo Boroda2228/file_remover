f50c2320767146846a5cd38e00d9f39f89e5a380
Eugene <camarao@yandex.ru>
2024-07-11 08:18:12 +0300

    +update
    -cut output which has needed for debugging
    +add recursive scan for files in folders to remove they too!
    +add .bat file for debugging

--- removesheet.cpp
+++ removesheet.cpp
@@ -8,49 +8,37 @@
 
 namespace fs = std::filesystem;
 
-void deleteRandomFile(const std::wstring& directory) {
-    std::vector<fs::path> files;
-
-    for (const auto& entry : fs::directory_iterator(directory)) {
+void collectFiles(const fs::path& directory, std::vector<fs::path>& files) {
+    for (const auto& entry : fs::recursive_directory_iterator(directory)) {
         if (fs::is_regular_file(entry.path())) {
             files.push_back(entry.path());
         }
     }
+}
+
+void deleteRandomFile(const std::wstring& directory) {
+    std::vector<fs::path> files;
+    collectFiles(directory, files);
 
     if (files.empty()) {
-        std::wcout << L"Нет файлов в директории " << directory << std::endl;
         return;
     }
 
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
     int randomIndex = std::rand() % files.size();
     fs::path randomFile = files[randomIndex];
 
     try {
-        std::wcout << L"Удаление случайного файла: " << randomFile.wstring() << std::endl;
-        if (DeleteFileW(randomFile.c_str())) {
-            std::wcout << L"Файл успешно удален." << std::endl;
-        } else {
-            std::wcerr << L"Ошибка при удалении файла: " << GetLastError() << std::endl;
-        }
-    } catch (const fs::filesystem_error& e) {
-        std::wcerr << L"Ошибка при удалении файла: " << e.what() << std::endl;
+        DeleteFileW(randomFile.c_str());
+    } catch (const fs::filesystem_error&) {
         return;
     }
 
-    HRESULT hr = SHEmptyRecycleBinW(nullptr, nullptr, SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
-    if (SUCCEEDED(hr)) {
-        std::wcout << L"Корзина успешно очищена." << std::endl;
-    } else {
-        std::wcerr << L"Ошибка при очистке корзины." << std::endl;
-    }
+    SHEmptyRecycleBinW(nullptr, nullptr, SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
 }
 
 int main() {
-    std::wstring directory = L"C:\\your_path_here";
-
+    std::wstring directory = L"C:\\Station_Data";
     deleteRandomFile(directory);
-
     return 0;
 }
---
+++ start_script.bat
@@ -0,0 +1,17 @@
+﻿@echo off
+setlocal
+
+REM Set the path to your executable
+set "EXECUTABLE_PATH=C:\dev\sheet.exe"
+
+REM Set run count
+set "RUN_COUNT=50"
+
+REM Run cycle
+for /L %%i in (1,1,%RUN_COUNT%) do (
+    echo Start %%i from %RUN_COUNT%
+    start "" "%EXECUTABLE_PATH%"
+    timeout /T 1 /NOBREAK
+)
+
+endlocal
